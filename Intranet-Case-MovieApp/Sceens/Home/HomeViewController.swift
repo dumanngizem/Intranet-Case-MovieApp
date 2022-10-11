8661f3fe5f9846115e570a449dc6b6143eaa33d2
Gizem Duman <gduman6@hotmail.com>
2022-10-11 21:08:22 +0300

    configure folder design project

---
+++ Intranet-Case-MovieApp/Sceens/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Intranet-Case-MovieApp
+//
+//  Created by Gizem Duman on 11.10.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
