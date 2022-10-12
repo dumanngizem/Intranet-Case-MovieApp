32cd2b2fd2f28f33cc80c38f33a5d042f9593c1a
Gizem Duman <gduman16@hotmail.com>
2022-10-12 19:52:56 +0300

    MOVAPP-04 add MovieDetailBuilder

---
+++ Intranet-Case-MovieApp/Sceens/MovieDetail/MovieDetailBuilder.swift
@@ -0,0 +1,22 @@
+//
+//  MovieDetailBuilder.swift
+//  Intranet-Case-MovieApp
+//
+//  Created by Gizem Duman on 12.10.2022.
+//
+
+import Foundation
+import UIKit
+
+final class MovieDetailBuilder {
+    
+    static func make() -> MovieDetailViewController {
+        let storyBoard = UIStoryboard(name: "MovieDetail", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "MovieDetail") as! MovieDetailViewController
+        let services = MoviesServices()
+        let repository = MoviesRepository(moviesServices: services)
+        let viewModel = MovieDetailViewModel(repository: repository)
+        vc.viewModel = viewModel
+        return vc
+    }
+}
